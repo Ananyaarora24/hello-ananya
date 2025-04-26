bc059cf8755a8f64b4bce3b3aa81ac490dd67e0a
Ananyaarora24 <156628726+Ananyaarora24@users.noreply.github.com>
2025-04-26 15:24:06 -0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20%' }}>
+      <h1>Hello, Ananya!</h1>
+      <p>Welcome to my website 🎉</p>
+    </div>
+  );
+}
+
+export default App;
